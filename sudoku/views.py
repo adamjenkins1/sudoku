dd7dcfb1020cbe4e25b4bd20d3eaeaf4f31267e7
Adam Jenkins <adamjenkins@comcast.net>
2017-11-05 18:50:21 -0800

    Reads initial board and calls AC3 executable
    now just have to write solved board to array and return that in the
    HttpResponse()

--- sudoku/views.py
+++ sudoku/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic.base import RedirectView
-import json
+import os
+import sys
+import subprocess
 
 def index(request, boardSize, diff):
     selected = 'home'
@@ -28,8 +30,23 @@ def about(request):
     return render(request, 'about.html', {'selected': selected})
 
 def solve(request):
-    ret = request.POST
     initialBoard = []
-    initialBoard = ret.getlist('board[]')
-    print(initialBoard)
+    initialBoard = request.POST.getlist('board[]')
+    with open('sudoku/static/c++/temp.txt', 'w') as f:
+        f.write(' '.join(initialBoard))
+    boardSize = int((len(initialBoard))**(0.5))
+    p = subprocess.Popen(['sudoku/static/c++/AC3', str(boardSize), 'sudoku/static/c++/temp.txt'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, error = p.communicate()
+    error = error.decode()[:-1]
+    if(error != ''):
+        print('Error: ' + error)
+
+    out = out.decode()[:-1]
+    if(out[0] != '1'):
+        print('AC3 encoutered an error')
+
+    solvedBoard = out[1:] 
+
+    print(' '.join(initialBoard))
+    print(solvedBoard)
     return HttpResponse()
