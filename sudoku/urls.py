bfab74ba85573cefe8ae54f0e50010502d82e66a
Adam Jenkins <adamjenkins@comcast.net>
2017-11-09 21:16:21 -0800

    Adds correct urls.py and ignores /data

--- sudoku/urls.py
+++ sudoku/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url = '/9/m/')),
     url(r'^([0-9]+)/([a-z]+)/$', views.index, name = 'index'),
+    url(r'^data/$', views.data, name = 'data'),
+    url(r'^([0-9]+)/([a-z]+)/save/$', views.saveData, name = 'save'),
     url(r'^solve/$', views.solve, name = 'solve'),
     url(r'^about/$', views.about, name = 'about'),
 ]
