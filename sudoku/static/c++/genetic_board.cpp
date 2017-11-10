491e1658c249a1b73c308d723c81016032494775
Caleb Alexander <calexander13@mail.csuchico.edu>
2017-11-10 09:41:23 -0800

    Implements a few functions in genetic_board.cpp
    The Genetic_board::h() is adapted fromAC3::is_solved()

--- sudoku/static/c++/genetic_board.cpp
+++ sudoku/static/c++/genetic_board.cpp
@@ -8,7 +8,9 @@
 /**
  * @brief     default constructor for a board
  */
-Genetic_board::Genetic_board(const std::vector<std::vector<int> > &board) {
+Genetic_board::Genetic_board(const std::vector<std::vector<int> > &board)
+  : board(board)
+  , size(board.size()) {
 
 }
 
@@ -48,14 +50,20 @@ void Genetic_board::set_row(std::vector<int> vals) {
  * @brief     trades columns with the other board
  */
 void Genetic_board::trade_cols(std::vector<int> indexes, Genetic_board& other) {
-
+  for (auto index: indexes) {
+    for (int i = 0; i < size; ++i) {
+      std::swap(board[i][index], other.board[i][index]);
+    }
+  }
 }
 
 /**
  * @brief     trades rows with the other board
  */
 void Genetic_board::trade_rows(std::vector<int> indexes, Genetic_board& other) {
-
+  for (auto index: indexes) {
+    board[index].swap(other[index]);
+  }
 }
 
 /**
@@ -63,7 +71,35 @@ void Genetic_board::trade_rows(std::vector<int> indexes, Genetic_board& other) {
  * @retur     a value, H, between [0, 3*sqrt(size)]
  */
 void Genetic_board::h() {
+  H = 0;
+  //check to see if the rows and columns are all valid
+  std::vector<bool> rowset(size, 0), colset(size, 0);
+  for (int i = 0; i < size; ++i) {
+    for (int j = 0; j < size; ++j) {
+      if (board[i][j].value)
+        rowset[board[i][j].value-1] = 1;
+      if (board[j][i].value)
+        colset[board[j][i].value-1] = 1;
+    }
+    if (!all(rowset)) H++;
+    if (all(colset)) H++;
+    rowset.clear(); colset.clear();
+  }
 
+  std::vector<bool> block(size, 0);
+  int ssize = std::sqrt(size);
+  for (int brow = 0; brow < ssize; ++brow) {
+    for (int bcol = 0; bcol < ssize; ++bcol) {
+      for (int row = 0; row < ssize; ++row) {
+        for (int col = 0; col < ssize; ++col) {
+          if (board[brow*ssize+row][bcol*ssize+col].value)
+            block[board[brow*ssize+row][bcol*ssize+col]].value-1] = 1;
+        }
+      }
+      if (!all(block)) H++;
+      block.clear();
+    }
+  }
 }
 
 /**
